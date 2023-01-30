92386b4c79078566d2c16d9494135a6fe795526e
Godswill Francis <willzudoh4@gmail.com>
2023-01-30 10:17:34 -0800

    added preloader

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -3,18 +3,46 @@ import HeroSwiper from '../component/HeroSwiper';
 import Navbar from '../component/Navbar';
 import { Creators, Footer, HeroHome, SectionListComponent } from '../sections';
 import Featured from '../sections/Featured';
+import React, { useState } from 'react';
+import Image from 'next/image';
 
 export default function Homepage() {
+  const [isLoading, setLoading] = useState(true);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2400);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  });
+
   return (
     <div>
-      <Navbar />
-      <HeroHome />
-      <HeroSwiper />
-      <Featured />
-      <UpcomingAuction />
-      <SectionListComponent />
-      <Creators />
-      <Footer />
+      {isLoading ? (
+        <div className="preloader">
+          <Image
+            src="/shopping bag animation_02.gif"
+            alt="shopping_preloader"
+            width={150}
+            height={100}
+            className="preloader_img"
+          />
+        </div>
+      ) : (
+        <div>
+          <Navbar />
+          <HeroHome />
+          <HeroSwiper />
+          <Featured />
+          <UpcomingAuction />
+          <SectionListComponent />
+          <Creators />
+          <Footer />
+        </div>
+      )}
     </div>
   );
 }
--- pages/Success.tsx
+++ pages/Success.tsx
@@ -7,34 +7,59 @@ import style from '../styles/Success.module.css';
 type Props = {};
 
 function Success({}: Props) {
+  const [isLoading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2400);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  });
   return (
     <div>
-      <HeadComponent />
-      <Navbar />
-      <div className={style.congrats}>
-        <div className={style.color1}></div>
-        <div className={style.color2}></div>
-        <div className={style.color3}></div>
-        <Image
-          src="/Woman get online delivery.png"
-          alt="Congrats image"
-          width={454}
-          height={405}
-          className={style.congratsImage}
-        />
-        <h2>Hey Celestina, thank you for your purchase. </h2>
-        <div>
-          <p>
-            You are amazing. Cheers to being <span>ARTSY!</span>
-          </p>
+      {isLoading ? (
+        <div className="preloader">
           <Image
-            src="/noto_party-popper.png"
-            alt="party"
-            width={32}
-            height={33}
+            src="/shopping bag animation_02.gif"
+            alt="shopping_preloader"
+            width={150}
+            height={100}
+            className="preloader_img"
           />
         </div>
-      </div>
+      ) : (
+        <div>
+          <HeadComponent />
+          <Navbar />
+          <div className={style.congrats}>
+            <div className={style.color1}></div>
+            <div className={style.color2}></div>
+            <div className={style.color3}></div>
+            <Image
+              src="/Woman get online delivery.png"
+              alt="Congrats image"
+              width={454}
+              height={405}
+              className={style.congratsImage}
+            />
+            <h2>Hey Celestina, thank you for your purchase. </h2>
+            <div>
+              <p>
+                You are amazing. Cheers to being <span>ARTSY!</span>
+              </p>
+              <Image
+                src="/noto_party-popper.png"
+                alt="party"
+                width={32}
+                height={33}
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
